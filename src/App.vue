f0d25125717bca7df70a8adcfe703530ed5cfd88
jaisoneji <jai.soneji@gmail.com>
2020-05-13 20:16:40 +0530

    Initial setup of Project with Components added

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template >
+  <div id="app" class="bg-background-primary h-screen" :class="theme">
+    <div class="navbar bg-background-secondary" >
+      <img src="https://img.icons8.com/nolan/64/coronavirus.png" class="p-2 mx-auto"/>
+    </div>
+    <Cards/>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Cards from './components/Cards.vue'
+import Chart from './components/Chart.vue'
+
+// import './assets/css/main.css'
+
+export default {
+  name: 'App',
+  components: {
+    Cards,
+    Chart
+  },
+  data(){
+    return{
+      theme:'theme-dark'
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/css/main.css';
+</style>
