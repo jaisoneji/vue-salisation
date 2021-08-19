805de1800409f09aa90099d19d285e364deb2110
jaisoneji <jai.soneji@gmail.com>
2021-08-19 15:00:00 +0530

    Updated graph

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// You can use your own logic to set your local or production domain
 const baseDomain = "https://api.rootnet.in/covid19-in";
-// The base URL is empty this time due we are using the jsonplaceholder API
 const baseURL = `${baseDomain}`;
 
 export default axios.create({
